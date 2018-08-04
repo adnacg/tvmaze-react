fa656c26049ccec524cb50cfe6e74c14a25c9980
adna <adna@adna.com>
2018-08-04 16:36:13 +0800

    Switch to Ajax

---
+++ home.js
@@ -0,0 +1,33 @@
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      query: "",
+      movies: []
+    };
+  }
+
+  changeHandler = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  clickHandler = async () => {
+    const response = await fetch(
+      "http://api.tvmaze.com/search/shows?q=" + this.state.query
+    );
+    const data = await response.json();
+    this.setState({ movies: data });
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <p className="title">TV Maze</p>
+        <Search onChange={this.changeHandler} onClick={this.clickHandler} />
+        <Results movies={this.state.movies} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"));
---
+++ search.js
@@ -0,0 +1,12 @@
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <input onChange={this.props.onChange} className="querybar" />
+        <button onClick={this.props.onClick} className="btn">
+          Search
+        </button>
+      </div>
+    );
+  }
+}
